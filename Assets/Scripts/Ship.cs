e960af50621fff87e2f6d327dcd5bdcb4a070b8f
YebedlievTimofyovitch <57999987+YebedlievTimofyovitch@users.noreply.github.com>
2020-08-03 18:59:24 +0300

    prototyping environment

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -13,7 +13,6 @@ public class Ship : MonoBehaviour
     private void Start()
     {
         Application.targetFrameRate = 30;
-        QualitySettings.vSyncCount = 0;
     }
 
     private void Update()
