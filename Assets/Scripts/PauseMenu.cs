996d812665fb1cbdbc7f6b6388b170d39c6beec4
YebedlievTimofyovitch <57999987+YebedlievTimofyovitch@users.noreply.github.com>
2020-08-17 18:47:31 +0300

    added pause menu and main menu with full functionality

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject PauseMenuObject = null;
+    [SerializeField] private Image CanvasColor = null;
+    private bool isPaused = false;
+
+    // Update is called once per frame
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void PauseGame()
     {
-        
+        if (!isPaused)
+        {
+            CanvasColor.color = new Color(0f, 0f, 0f, 0.25f);
+            Time.timeScale = 0.0f;
+            PauseMenuObject.SetActive(true);
+            isPaused = true;
+        }
+        else
+        {
+            CanvasColor.color = new Color(0f, 0f, 0f, 0f);
+            Time.timeScale = 1.0f;
+            PauseMenuObject.SetActive(false);
+            isPaused = false;
+        }
+    }
+
+    public void ReturnToGame()
+    {
+        CanvasColor.color = new Color(0f, 0f, 0f, 0f);
+        Time.timeScale = 1.0f;
+        PauseMenuObject.SetActive(false);
+        isPaused = false;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        CanvasColor.color = new Color(0f, 0f, 0f, 0f);
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
