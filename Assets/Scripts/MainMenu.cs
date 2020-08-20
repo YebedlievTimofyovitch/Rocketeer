df4e289c555d8efe9a2c8af0967899ac147d5aaf
YebedlievTimofyovitch <57999987+YebedlievTimofyovitch@users.noreply.github.com>
2020-08-20 18:15:55 +0300

    added the ability to reset the highscore

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Text HighScoreMenuText = null;
+
+    private void Start()
+    {
+        HighSoreMainMenu();
+    }
+
+    private void HighSoreMainMenu()
+    {
+        HighScoreMenuText.text = @"High Score
+_________
+" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+
     public void LoadGameLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
@@ -13,5 +28,14 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void EraseHighScore()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+
+        HighScoreMenuText.text = @"High Score
+_________
+0";
     }
 }
